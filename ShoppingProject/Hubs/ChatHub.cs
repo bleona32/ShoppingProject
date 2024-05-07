6410e81ab6890a32cd3704328f80c014ff36e704
Bleona Rrustemi <104694807+bleona32@users.noreply.github.com>
2024-05-07 21:26:05 +0200

    ChatHub

---
+++ ShoppingProject/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using ShoppingProject.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingProject.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.All.SendAsync("RecieveMessage", name, message);
+        }
+    }
+}
