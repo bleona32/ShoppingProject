341a0e0d34379de8b19c2bfb22af40afba2d7d0c
Bleona Rrustemi <104694807+bleona32@users.noreply.github.com>
2024-05-14 21:21:47 +0200

    propertis

---
+++ ShoppingProject/wwwroot/js/chat.js
@@ -0,0 +1,105 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+
+connection.on("RecieveMessage", function (user, message) {
+
+    var div = document.getElementById("Chats");
+    var p = document.createElement("p");
+    var h = document.createElement("h4");
+    var res = user.substring(0, 5);
+    p.innerText = res;
+    h.innerText = message;
+    div.appendChild(p);
+    div.appendChild(h);
+
+
+});
+
+document.getElementById("SendMessageButton").addEventListener("click", function (event) {
+    var user = document.getElementById("name").value;
+    var message = document.getElementById("inputText").value;
+    var currentdate = new Date();
+
+
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+
+
+});
+/*class Message {
+    constructor(username, text, when) {
+        this.userName = username;
+        this.text = text;
+        this.when = when;
+    }
+}
+
+// userName is declared in razor page.
+const username = userName;
+const textInput = document.getElementById('messageText');
+const whenInput = document.getElementById('when');
+const chat = document.getElementById('chat');
+const messagesQueue = [];
+
+document.getElementById('submitButton').addEventListener('click', () => {
+    var currentdate = new Date();
+    when.innerHTML =
+        (currentdate.getMonth() + 1) + "/"
+        + currentdate.getDate() + "/"
+        + currentdate.getFullYear() + " "
+        + currentdate.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+});
+
+function clearInputField() {
+    messagesQueue.push(textInput.value);
+    textInput.value = "";
+}
+
+function sendMessage() {
+    let text = messagesQueue.shift() || "";
+    if (text.trim() === "") return;
+    
+    let when = new Date();
+    let message = new Message(username, text);
+    sendMessageToHub(message);
+}
+
+function addMessageToChat(message) {
+    let isCurrentUserMessage = message.userName === username;
+
+    let container = document.createElement('div');
+    container.className = isCurrentUserMessage ? "container darker" : "container";
+
+    let sender = document.createElement('p');
+    sender.className = "sender";
+    sender.innerHTML = message.userName;
+    let text = document.createElement('p');
+    text.innerHTML = message.text;
+
+    let when = document.createElement('span');
+    when.className = isCurrentUserMessage ? "time-left" : "time-right";
+    var currentdate = new Date();
+    when.innerHTML = 
+        (currentdate.getMonth() + 1) + "/"
+        + currentdate.getDate() + "/"
+        + currentdate.getFullYear() + " "
+        + currentdate.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+
+    container.appendChild(sender);
+    container.appendChild(text);
+    container.appendChild(when);
+    chat.appendChild(container);
+}
+*/
+
